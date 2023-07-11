ce8884b835d24fc611c9e3c4c4fe6126e01ffb1d
Shaine <shaine99waters@gmail.com>
2023-07-11 16:24:48 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+
+function returnFirstTwoDrivers(){
+    const returnFirstTwoDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    return returnFirstTwoDrivers.slice(0,2)
+}
+
+//2
+function returnLastTwoDrivers(){
+    const returnLastTwoDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    return returnLastTwoDrivers.slice(2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(integer){
+    return function fareQuintupler() {
+         return integer*integer;
+    }    
+}
+    
+function fareDoubler(integer){
+    return integer*2;
+}
+
+function fareTripler(integer){
+    return integer*3;
+}
+
+function selectDifferentDrivers(arrayOfDrivers,returnFirstTwoDrivers){
+   return returnFirstTwoDrivers(arrayOfDrivers)
+   
+    // const returnFirstTwoDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    // return returnFirstTwoDrivers.slice(0,2);
+
+    
+}
